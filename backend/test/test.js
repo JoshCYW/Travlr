f6153eb42214f9ed8693b0b6c64650f6e1ed1e63
CheyenneKoh <60183723+CheyenneKoh@users.noreply.github.com>
2020-04-11 00:00:07 +0800

    assigngovernment test

--- backend/test/test.js
+++ backend/test/test.js
@@ -5,27 +5,31 @@ const Immigration = artifacts.require('Immigration');
 const EthPassport = artifacts.require('EthPassport');
 
 contract('Travlr', function(accounts) {
-    let _travlrOwner = accounts[0];
-    let _sggovt = accounts[1];
+    let _sggovt = accounts[2];
+    let travlrOwner;
     let travlrInstance;
     let governmentAddress;
+    let governmentInstance;
 
 
     it('Deploy travlr', async () => {
         travlrInstance = await Travlr.deployed();
-        console.log("Travlr contract address: "+ travlrInstance.address);
-        console.log(travlrInstance.checkOwner());
-        console.log(_sggovt);
+        travlrOwner = await travlrInstance.owner();
+        console.log("This is account 0: " + accounts[0])
+        console.log("Travlr Owner: " + travlrOwner);
+        console.log("Travlr contract address: " + travlrInstance.address)
+        console.log("SG govt: " + _sggovt);
         assert(travlrInstance.address !== '')
     });
-    //
-    // it('Assign government', async () => {
-    //     governmentAddress = await travlrInstance.assignGovernment(_sggovt,65, {
-    //         from: travlrInstance.travlrOwner});
-    //     console.log(governmentAddress);
-    //     assert(governmentAddress !== '')
-    //
-    // });
+
+    it('Assign government', async () => {
+        governmentAddress = await travlrInstance.assignGovernment(_sggovt,65, {
+            from: travlrOwner});
+        console.log(governmentAddress);
+        assert(governmentAddress !== '')
+
+    });
+
 
 })
 
